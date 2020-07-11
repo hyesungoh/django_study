f0c1739b99b54e6b5ca850d6dfe35e1f2cf086bb
hyesungoh <haesungoh414@gmail.com>
2020-07-12 00:38:27 +0900

    base setting

--- crud4likelion/projectname/appname/views.py
+++ crud4likelion/projectname/appname/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def main(request):
     return render(request, 'appname/main.html')
+
+def create(request):
+    return render(request, 'appname/create.html')
+
+def read(request):
+    return redirect('main')
+
+def update(request):
+    return redirect('main')
+
+def delete(request):
+    return redirect('main')
