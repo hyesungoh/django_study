0172f06b4989aeec50af5b5b8f3ab2b333d6bab7
hyesungoh <haesungoh414@gmail.com>
2020-07-11 23:26:17 +0900

    Create appname/main

--- crud4likelion/projectname/projectname/urls.py
+++ crud4likelion/projectname/projectname/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import appname.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', appname.views.main, name='main'),
 ]
