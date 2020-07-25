6ca091b8d831e3e2de5df18f27c202fbdcdd86d3
hyesungoh <haesungoh414@gmail.com>
2020-07-25 18:59:24 +0900

    change wrong spell

--- crud4likelion/projectname/appname/models.py
+++ crud4likelion/projectname/appname/models.py
@@ -10,16 +10,16 @@ class Post(models.Model):
     content = models.TextField()
     image = models.ImageField(upload_to='images/', blank=True)
 
+class Comment(models.Model):
+    def __str__(self):
+        return self.
+
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.CharField(max_length=50)
+
 class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
     nickname = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=20)
-
-def Comment(models.Model):
-    def __str__(self):
-        return self.
-
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.CharField(max_length=50)
